3f572954e8f9c67b2bbacbb4d3deeeabd420cd2f
yjb23tech <yjb23tech@gmail.com>
2024-01-10 13:43:54 +0000

    Feel so alive

---
+++ App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+    return (
+        <div>
+            <h1>Sanity Check (But We Still Pushing P)</h1>
+        </div>
+    )
+}
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<App />)
+
