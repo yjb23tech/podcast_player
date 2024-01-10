9ff7229740fceb8ae9bf63c1a849fee691d80192
yjb23tech <yjb23tech@gmail.com>
2024-01-10 14:19:34 +0000

    Pluto

--- App.jsx
+++ App.jsx
@@ -5,6 +5,12 @@ const App = () => {
     return (
         <div>
             <h1>Sanity Check (But We Still Pushing P)</h1>
+            <Card 
+                pp_title = "Prometheus Rising"
+                pp_description = "Above It All"
+                pp_number_of_likes = "1 like"
+                pp_number_of_comments = "1 comment"
+            />
         </div>
     )
 }
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,7 +1,32 @@
+import pencil_icon from "../public/images/pencil_icon.png"
+import trash_icon from "../public/images/trash_icon.png"
+import heart_icon from "../public/images/heart_icon.png"
+import comment_icon from "../public/images/comment_icon.png"
+
 const Card = (props) => {
     return (
-        <div>
+        <div className="pp-card">
+            <div className="pp-row-title">
+                <h2>{props.pp_title}</h2>
+                <div className="pp-edit-delete-icons">
+                    <img src={pencil_icon} />
+                    <img src={trash_icon} />
+                </div>
+            </div>
+            <div className="pp-row-description">
+                <p>{props.pp_description}</p>
+            </div>
+            <div className="pp-row-blank">
 
+            </div>
+            <div className="pp-row-like-comment-text">
+                <p>{props.pp_number_of_likes}</p>
+                <p>{props.pp_number_of_comments}</p>
+            </div>
+            <div className="pp-row-like-comment-icons">
+                <img src={heart_icon} />
+                <img src={comment_icon} />
+            </div>
         </div>
     )
 }
